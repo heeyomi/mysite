058fa618f3582041a5de6715c899ef50e4070e18
heeyomi <heeyomi@heeyomi>
2021-05-27 11:57:41 +0900

    user #1 + serverside include

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.douzone.mysite.controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,17 +9,22 @@ import javax.servlet.http.HttpServletResponse;
 import com.douzone.mysite.mvc.util.MvcUtils;
 
 
-public class MainController extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String action = request.getParameter("a");
 		
-		MvcUtils.forward("main/index", request, response);
+		if ("joinform".equals(action)) {
+			MvcUtils.forward("user/joinform", request, response);
+		} else {
+			MvcUtils.redirect("/mysite02", request, response);
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
+
 }
