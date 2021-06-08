88c5b3f5e38f4146bb2a67253d26e15cf05ad584
heeyomi <heeyomi@heeyomi>
2021-06-08 11:28:22 +0900

    관리자 페이지 #1

--- mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
@@ -3,12 +3,15 @@ package com.douzone.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class );
 	
 	@ExceptionHandler(Exception.class)
 	public String handlerException(Model model, Exception e) {
@@ -16,8 +19,20 @@ public class GlobalExceptionHandler {
 		System.out.println(e);
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		System.out.println(errors);
-		//2. 사과 페이지
+		//System.out.println(errors);
+		LOGGER.error(errors.toString());
+		
+		/**
+		 * 1. appender
+		 *    - file appender 1개 이름은 /log-mysite/exception.log
+		 *      10M(Archiving 정책)
+		 *      1-10(rolling)
+		 *    - console appender
+		 * 2. logger - com.douzone.mysite.exception, level(error), (console+file) appender
+		 *    logger - Root, level(debug), console appender
+		 *    
+		 */
+		//2. 사과 페이지 
 		//3. 정상 종료
 		model.addAttribute("exception", errors.toString());
 		return "error/exception";
